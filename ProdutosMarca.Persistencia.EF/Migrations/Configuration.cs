47c31a007ea973f0a1db8f4ba23d3c4a625b019a
Willian <Willian@DESKTOP-K6JU1G7>
2018-11-22 15:27:27 -0200

    Inicio_Migrations

---
+++ ProdutosMarca.Persistencia.EF/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ProdutosMarca.Persistencia.EF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProdutosMarca.Persistencia.EF.Context.ProdutosMarcasDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProdutosMarca.Persistencia.EF.Context.ProdutosMarcasDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
