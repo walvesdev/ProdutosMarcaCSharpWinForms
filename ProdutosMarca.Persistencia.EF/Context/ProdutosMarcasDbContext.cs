b09621a29483126d19061ad49d033ab94661a506
Willian <Willian@DESKTOP-K6JU1G7>
2018-11-22 14:41:39 -0200

    Adicionar arquivos de projeto.

---
+++ ProdutosMarca.Persistencia.EF/Context/ProdutosMarcasDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ProdutosMarcas.Dominio;
+using System.Data.Entity;
+
+namespace ProdutosMarca.Persistencia.EF.Context
+{
+    public class ProdutosMarcasDbContext : DbContext 
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .HasRequired(p => p.Marca) //um produto requer um marca
+                .WithMany(p => p.Produtos) //marca pode ter varios produtos
+                .HasForeignKey(fk => fk.MarcaId) //chave estrangeira MarcaId
+                .WillCascadeOnDelete(false); // exclusao em cascata desativada
+        }
+    }
+}
