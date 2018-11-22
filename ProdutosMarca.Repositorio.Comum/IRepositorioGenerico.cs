931f2fc2a7c5e5abc841505bc3b8efc7791c99c9
Willian <Willian@DESKTOP-K6JU1G7>
2018-11-22 15:50:02 -0200

    Repo_1

---
+++ ProdutosMarca.Repositorio.Comum/IRepositorioGenerico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosMarca.Repositorio.Comum
+{
+    public interface IRepositorioGenerico<T>
+    {
+        List<T> SelecionarTodos();
+        T SelecionarPorId(T id);
+        void Inserir(T entidade);
+        void Atualizar(T entidade);
+        void Excluir(T entidade);
+
+    }
+}
